51878532ed1f3bd3d9f33e0c52e5e5d964e572d0
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-08-01 19:42:28 +0200

    Paint translated words and remove them

--- src/components/WordsText/WordsText.js
+++ src/components/WordsText/WordsText.js
@@ -4,7 +4,18 @@ import Word from '../Word/Word';
 
 const WordsText = ( props ) => {
     
-    return ();
+    return ( props.data.map((word, index) => {
+            return (
+              <Word
+                id={index}
+                key={index}
+                clicked={props.onClickHandle}
+                word={word}
+                translation={props.translations[word]}
+              />
+            );
+          })
+    );
 }
 
 
